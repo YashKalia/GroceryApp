825ff791ad37b43277f10df561180fcbdaf8b80a
Yash Kalia <yashkalia2@gmail.com>
2024-08-10 20:14:22 +0200

    Added missing fields in item obj

--- groceries/Backend/src/main/java/dev/yashkalia/groceries/Models/Items/Item.java
+++ groceries/Backend/src/main/java/dev/yashkalia/groceries/Models/Items/Item.java
@@ -1,7 +1,6 @@
 package dev.yashkalia.groceries.Models.Items;
 
 import lombok.*;
-import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection="items")
@@ -13,26 +12,28 @@ import org.springframework.data.mongodb.core.mapping.Document;
 public class Item {
 
 //    @Id
-    private String itemName;
-    private Integer price;
-    private String itemType;
+    private String ItemName;
+    private Integer Price;
+    private String ItemType;
     private String id;
-    private Integer age;
+    private Integer Age;
     private String DiscountId;
     private Boolean IsPack;
     private Integer Weight;
 
+
     @Override
     public String toString() {
         return "Item{" +
-                "id='" + id + '\'' +
-                ", ItemName='" + itemName + '\'' +
-                ", Price=" + price +
-                ", ItemType='" + itemType + '\'' +
-                ", Age=" + age +
+                "Price=" + Price +
+                ", ItemType='" + ItemType + '\'' +
+                ", id='" + id + '\'' +
+                ", Age=" + Age +
+                ", DiscountId='" + DiscountId + '\'' +
+                ", IsPack=" + IsPack +
+                ", Weight=" + Weight +
+                ", ItemName='" + ItemName + '\'' +
                 '}';
     }
-
-
 }
 
